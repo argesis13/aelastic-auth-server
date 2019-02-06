54c802a69cd00c0b96cfed1fe770239fff248396
swappteam <argesis13@yahoo.com>
2019-02-06 17:18:47 +0200

    initial project setup

---
+++ src/main/java/com/aelastic/aelasticauthserver/AelasticAuthServerApplication.java
@@ -0,0 +1,14 @@
+package com.aelastic.aelasticauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AelasticAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AelasticAuthServerApplication.class, args);
+    }
+
+}
+
